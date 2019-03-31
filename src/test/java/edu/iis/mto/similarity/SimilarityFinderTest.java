df902cf3f836fbda0f83e003213a608b9858b987
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-31 23:18:39 +0200

    utworzenie testu jaccardIndexForEmptySets()

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,47 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+
+import static edu.iis.mto.search.SearchResult.builder;
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+
+    private SimilarityFinder similarityFinder;
+
+    @Before public void initialize() {
+
+        similarityFinder = new SimilarityFinder((key, seq) -> {
+
+            SearchResult.Builder builder = builder();
+
+            for(int i = 0; i < seq.length; i++){
+                if (seq[i] == key){
+                    builder.withPosition(i);
+                    builder.withFound(true);
+                    return builder.build();
+                }
+            }
+
+            builder.withFound(false);
+            return builder.build();
+
+        });
+
+    }
+
+    @Test public void jaccardIndexForEmptySets() {
+
+        int[] setA = {};
+        int[] setB = {};
+
+        double result = similarityFinder.calculateJackardSimilarity(setA, setB);
+
+        assertThat(result, Is.is(1.0));
+
+    }
+
+}
